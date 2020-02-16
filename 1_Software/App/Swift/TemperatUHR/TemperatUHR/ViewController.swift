7593d903b54d1dc96b331d7e343b256ddeaf810c
Paul Goldschmidt <paul.goldschmidt@web.de>
2020-02-16 17:37:05 +0100

    added (old) swift files

---
+++ 1_Software/App/Swift/TemperatUHR/TemperatUHR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemperatUHR
+//
+//  Created by Paul Goldschmidt on 22.01.20.
+//  Copyright © 2020 Paul Goldschmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
